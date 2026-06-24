a0c73826b453a730bd2d7b233904de3a41a3a35d
Gevorg <gevo14m@gmail.com>
2026-06-24 15:59:48 +0400

    changes

--- src/components/metrics/charts/SemiCircleChart.tsx
+++ src/components/metrics/charts/SemiCircleChart.tsx
@@ -64,7 +64,7 @@ function SemiCircleChart({ data, chartTitle }: SemiCircleChartProps) {
 
     series.slices.template.setAll({
       cornerRadius: 5,
-      tooltipText: "{category} - [bold]{value}[/]",
+      tooltipText: "{category}     [bold]{value}[/]",
     });
 
     if (chartTitle !== undefined) {
